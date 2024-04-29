114c1e100f386e4fcb67769a09140853de6ad03d
hhieun1604 <convitne722@gmail.com>
2024-04-29 14:21:43 +0700

    add model

---
+++ src/main/java/com/jspservlet/model/TinhThanhPho.java
@@ -0,0 +1,22 @@
+package com.jspservlet.model;
+
+
+public class TinhThanhPho extends AbstractModel<TinhThanhPho> {
+	private String matp;
+	private String name;
+	
+	public String getMatp() {
+		return matp;
+	}
+	public void setMatp(String matp) {
+		this.matp = matp;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ src/main/java/com/jspservlet/model/UserModel.java
@@ -0,0 +1,57 @@
+package com.jspservlet.model;
+
+public class UserModel extends AbstractModel<UserModel> {
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Long getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+	public RoleModel getRole() {
+		return role;
+	}
+	public void setRole(RoleModel role) {
+		this.role = role;
+	}
+	public String getNumberPhone() {
+		return numberPhone;
+	}
+	public void setNumberPhone(String numberPhone) {
+		this.numberPhone = numberPhone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRepeatPassword() {
+		return repeatPassword;
+	}
+	public void setRepeatPassword(String repeatPassword) {
+		this.repeatPassword = repeatPassword;
+}
